8c5439cb0b624d176e09c50384d7b6da479d226e
ilis137 <iliyasshahapure1@gmail.com>
2022-09-21 08:02:46 +0530

    initial commit

---
+++ src/main/java/com/javapractice/DeckOfCards.java
@@ -0,0 +1,17 @@
+package com.javapractice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DeckOfCards {
+    
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to deck of cards program!");
+    }
+
+}
